e35cd94ca7caad0eb6c48b409d4ef7068f61cd57
goni715 <halesgoni715149123@gmail.com>
2025-03-12 16:27:29 +0600

    create-random session

--- src/app/modules/RandomSession/randomSession.service.ts
+++ src/app/modules/RandomSession/randomSession.service.ts
@@ -1,9 +1,18 @@
+import QuizModel from "../Quiz/quiz.model";
+import RandomSessionModel from "./randomSession.model";
 
 
 
 const createRandomSessionService = async (loginUserId: string) => {
-    
-    return loginUserId;
+  //get the quizIds
+  const data = await QuizModel.find({}, "_id").lean();
+  const quizIds = data?.map((quiz) => quiz._id);
+
+  const result = await RandomSessionModel.create({
+    players: [loginUserId],
+    quizzes: quizIds
+  });
+  return result;
 }
 
 
